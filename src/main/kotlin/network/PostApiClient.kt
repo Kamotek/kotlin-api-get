b12503f63fcf648fb1fbd669df14f425f7077f63
Kamil Badowicz <kamotek123@gmail.com>
2025-06-02 09:42:03 +0200

    feat: implement service layer, that contains logic responsible for processing posts extracted from network layer

--- src/main/kotlin/network/PostApiClient.kt
+++ src/main/kotlin/network/PostApiClient.kt
@@ -4,34 +4,69 @@ import com.model.Post
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.SerializationException
 import org.slf4j.LoggerFactory
 
+
+/**
+ * PostApiClient
+ * This object is used to connect with our API endpoint and fetch our posts
+ */
+
+/*
+ i decided to use object keyword, because thanks to that i can use it as a singleton, without
+ worrying about creating some instances or something
+*/
 object PostApiClient {
     private val logger = LoggerFactory.getLogger("PostApiClient")
 
-    suspend fun getPosts(postApiUrl: String): List<Post> {
-        val httpClient = HttpClient(CIO) {
-            install(ContentNegotiation) {
-                json()
+    /*
+     thanks to initializing HttpClient here, our singleton does not have to
+     initialize it every time we call savePosts
+     */
+    private val httpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 10000
+        }
+    }
 
+    /**
+     * @param postApiUrl - it's our endpoint, that's stored in config file
+     * @return posts - list of deserialized posts
+     * if there are some problems with connection, it will simply return empty list
+     */
+    suspend fun getPosts(postApiUrl: String): List<Post> {
+        return try {
+            val response: HttpResponse = httpClient.get(postApiUrl)
+
+            if (response.status != HttpStatusCode.OK) {
+                logger.error("Failed to fetch posts. HTTP status: ${response.status}")
+                return emptyList()
+            }
+
+            try {
+                response.body()
+            } catch (e: SerializationException) {
+                logger.error("Failed to deserialize response body into List<Post>: ${e.message}", e)
+                emptyList()
             }
         }
-
-        val httpResponse: HttpResponse = httpClient.get(postApiUrl)
-        httpClient.close()
-
-        if(httpResponse.status != HttpStatusCode.OK){
-            logger.error("Failed to fetch posts: HTTP ${httpResponse.status}")
-            return emptyList()
+        catch (e: Exception) {
+            logger.error("Unexpected error when fetching posts: ${e.message}", e)
+            emptyList()
         }
+    }
 
-        val posts: List<Post> = httpResponse.body()
-
-        return posts
+    fun close() {
+        httpClient.close()
     }
 }
--- src/main/kotlin/service/PostService.kt
+++ src/main/kotlin/service/PostService.kt
@@ -1,4 +1,20 @@
 package com.service
 
-class PostService {
+import com.model.Post
+import com.network.PostApiClient
+
+/**
+ * PostService – class that works as a business layer for our post api fetcher
+ *
+ * @param apiClient – httpClient that downloads all the data from provided endpoint
+ */
+class PostService(
+    private val apiClient: PostApiClient
+) {
+    /**
+     * we're downloading all the posts from the api
+     */
+    suspend fun getAllPosts(apiUrl: String): List<Post> {
+        return apiClient.getPosts(apiUrl)
+    }
 }
